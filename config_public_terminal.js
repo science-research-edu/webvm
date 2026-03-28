c64ed3dd971902a135906ae6fc13fc4b608fa8d5
science-research-edu <ek2012mayday@gmail.com>
2026-03-28 18:05:42 -0400

    Update config_public_terminal.js

--- config_public_terminal.js
+++ config_public_terminal.js
@@ -1,5 +1,5 @@
 // The root filesystem location
-export const diskImageUrl = "wss://disks.webvm.io/debian_large_20230522_5044875331_2.ext2";
+export const diskImageUrl = "wss://disks.webvm.io/alpine_large_20230522_5044875331.ext2";
 // The root filesystem backend type
 export const diskImageType = "cloud";
 // Print an introduction message about the technology
@@ -9,7 +9,7 @@ export const needsDisplay = true;
 // Executable full path (Required)
 export const cmd = "/usr/bin/chromium";
 // Arguments, as an array (Required)
-export const args = ["--no-sandbox", "--disable-gpu", "https://google.com"];
+export const args = ["--no-sandbox", "https://google.com"];
 // Optional extra parameters
 export const opts = {
 	// Environment variables
