0acd16568cc7adaa7bb6564185492fa0e0432c95
science-research-edu <ek2012mayday@gmail.com>
2026-03-29 11:31:19 -0400

    Update config_public_terminal.js

--- config_public_terminal.js
+++ config_public_terminal.js
@@ -1,23 +1,16 @@
-// The root filesystem location
+// Point to the Alpine image which has the desktop and browser
 export const diskImageUrl = "wss://disks.webvm.io/alpine_large_20230522_5044875331.ext2";
-// The root filesystem backend type
 export const diskImageType = "cloud";
-// Print an introduction message about the technology
 export const printIntro = true;
-// Is a graphical display needed
-export const needsDisplay = true;
-// Executable full path (Required)
+export const needsDisplay = true; // REQUIRED FOR GRAPHICS
+
+// This opens Chromium to Google automatically
 export const cmd = "/usr/bin/chromium";
-// Arguments, as an array (Required)
-export const args = ["--no-sandbox", "https://google.com"];
-// Optional extra parameters
+export const args = ["--no-sandbox", "--disable-gpu", "https://google.com"];
+
 export const opts = {
-	// Environment variables
 	env: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
-	// Current working directory
 	cwd: "/home/user",
-	// User id
 	uid: 1000,
-	// Group id
 	gid: 1000
 };
